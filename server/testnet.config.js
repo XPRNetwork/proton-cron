b43fa0efa4d8fd5e885facf732714f46ed5a1190
Metallicus Dev <>
2021-01-13 03:01:13 -0400

    update pm2 scripts

--- server/testnet.config.js
+++ server/testnet.config.js
@@ -1,13 +1,12 @@
 module.exports = {
     apps : [
       {
-        name: 'proton-longstaking-testnet-bots',
+        name: 'proton-cron-testnet',
         script: 'dist/index.js',
         node_args : '-r dotenv/config',
         watch: false,
         env: {
-          'CHAIN': 'proton-test',
-          'ORACLE_INDEX': 2
+          'CHAIN': 'proton-test'
         }
       }
     ]
